f97d9043442a89749882637b38dd495030882473
wangkun <wb-wk228937@ant-financial.com>
2018-02-06 18:06:30 +0800

    [master]submit

---
+++ config/paths.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+const url = require('url');
+
+// Make sure any symlinks in the project folder are resolved:
+// https://github.com/facebookincubator/create-react-app/issues/637
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+module.exports = {
+  dotenv: resolveApp('.env'),
+  appBuild: resolveApp('build'),
+  appPublic: resolveApp('public'),
+  appHtml: resolveApp('public/index.html'),
+  appIndexJs: resolveApp('src/entries/*.js'),
+  appSrc: resolveApp('src'),
+  appNodeModules: resolveApp('node_modules'),
+};
